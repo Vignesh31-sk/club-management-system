cda5901295c153ec1dde6e296e05fe5ad89419d8
Vignesh S <vignesh31.sk@gmail.com>
2024-05-16 23:10:47 +0530

    Connected to mysql

--- src/club_management/settings.py
+++ src/club_management/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'club_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "clubs_db",
+        "USER": "django",
+        "PASSWORD": "club@123",
+        "HOST": "localhost",
+        "PORT": "3306"
     }
 }
 
