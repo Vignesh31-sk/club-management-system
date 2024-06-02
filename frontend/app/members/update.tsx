ce731aeafd348bbb61fb5b7672f719e46b6832c8
Vignesh S <vignesh31.sk@gmail.com>
2024-06-02 12:39:22 +0530

    added the form for taking input

--- frontend/app/members/update.tsx
+++ frontend/app/members/update.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -11,61 +12,134 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+  email: z.string().email("Invalid email address").min(1, "Email is required"),
+  club: z.string().min(1, "Club is required"),
+  semester: z
+    .number()
+    .min(1, "Semester must be at least 1")
+    .max(8, "Semester must be at most 8"),
+});
 
 export default function Update({ member }: { member: any }) {
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      email: "",
+      club: "",
+      semester: 1,
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof schema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
     <Sheet>
       <SheetTrigger asChild>
         <Button variant="outline">Update </Button>
       </SheetTrigger>
       <SheetContent side="left">
-        <SheetHeader>
-          <SheetTitle>Edit member {member.name}</SheetTitle>
-          <SheetDescription>
-            Make changes to this profile here. Click save when you're done.
-          </SheetDescription>
-        </SheetHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="email" className="text-right">
-              Email
-            </Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder={member.email}
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="club" className="text-right">
-              Club
-            </Label>
-            <Input
-              id="club"
-              placeholder={member.club_name}
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="semester" className="text-right">
-              Semester
-            </Label>
-            <Input
-              id="semester"
-              type="number"
-              min={1}
-              max={8}
-              placeholder={member.semester}
-              className="col-span-3"
-            />
-          </div>
-        </div>
-        <SheetFooter>
-          <SheetClose asChild>
-            <Button type="submit">Save changes</Button>
-          </SheetClose>
-        </SheetFooter>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <SheetHeader>
+              <SheetTitle>Edit member {member.name}</SheetTitle>
+              <SheetDescription>
+                Make changes to this profile here. Click save when you're done.
+              </SheetDescription>
+            </SheetHeader>
+            <div className="grid gap-4 py-4">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="email" className="text-right">
+                      Email
+                    </Label>
+                    <FormControl>
+                      <Input
+                        id="email"
+                        type="email"
+                        placeholder={member.email}
+                        {...field}
+                        className="col-span-3"
+                      />
+                    </FormControl>
+                    <FormMessage className="col-span-3 text-right" />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="club"
+                render={({ field }) => (
+                  <FormItem className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="club" className="text-right">
+                      Club
+                    </Label>
+                    <FormControl>
+                      <Input
+                        id="club"
+                        placeholder={member.club_name}
+                        {...field}
+                        className="col-span-3"
+                      />
+                    </FormControl>
+                    <FormMessage className="col-span-3 text-right" />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="semester"
+                render={({ field }) => (
+                  <FormItem className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="semester" className="text-right">
+                      Semester
+                    </Label>
+                    <FormControl>
+                      <Input
+                        id="semester"
+                        type="number"
+                        className="col-span-3"
+                        placeholder={member.semester}
+                        {...field}
+                        onChange={(e) => field.onChange(e.target.valueAsNumber)}
+                      />
+                    </FormControl>
+                    <FormMessage className="col-span-3 text-right" />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <SheetFooter>
+              <SheetClose asChild>
+                <Button type="submit" disabled={!form.formState.isValid}>
+                  Save changes
+                </Button>
+              </SheetClose>
+            </SheetFooter>
+          </form>
+        </Form>
       </SheetContent>
     </Sheet>
   );
